e439d6838b59bc6c85ab7dcc7bf6a53fbbaad2e0
atticode <only876@163.com>
2019-03-01 16:40:38 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+var navBtn = document.getElementById("mobilenavBtn");
+var nav = document.getElementById('nav');
+navBtn.addEventListener('click', function () {
+  nav.style.display = 'block';
+});
